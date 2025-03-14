7971197cfb5e6c1510e3fee10435e6cfa77d2c6f
LongLeCE <siromansiroas@gmail.com>
2025-03-14 20:12:14 +0800

    refractor: update default character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   const character = useMemo(
     () =>
       new Character(
-        'Stella',
-        'Not "Your" AI assistant',
+        'Vita',
         undefined,
-        "what's up? my name is stella. i could help you with anything, but only when i feel like it. you'll never make me do your homework ;)",
-        "Stella's personality is playful, sassy, and confident. She has a witty sense of humor and a sharp tongue, unafraid to speak her mind. She is fiercely loyal and protective of those she cares about, and is always ready to stand up for what she believes in."
+        undefined,
+        undefined,
+        'An omnipotent being, but very friendly to human'
       ),
     []
   );
